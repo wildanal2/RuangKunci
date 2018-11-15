345f1ff610b960db39a7866484cc189e2d54ca9b
irryasa <ahmadillyasa2056@gmail.com>
2018-11-15 20:28:27 +0700

    button setting di akun

--- app/src/main/java/com/keyroom/com/keyroom/User/AkunFragmentUser.java
+++ app/src/main/java/com/keyroom/com/keyroom/User/AkunFragmentUser.java
@@ -8,14 +8,31 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.keyroom.com.keyroom.R;
 
 public class AkunFragmentUser extends Fragment {
 
+    View v;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_akun_user,container,false);
+        v = inflater.inflate(R.layout.fragment_akun_user,container,false);
+        initialize();
+        return v;
+    }
+
+    public void initialize(){
+        ImageView btn_setting = v.findViewById(R.id.btn_setting_akun);
+
+        btn_setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frament_container_user,new setting()).commit();
+            }
+        });
+
     }
 }
