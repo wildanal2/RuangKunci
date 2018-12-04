454438c7a0c56e3245e10639e485795af1990aee
Pipingyacobus <44889801+Pipingyacobus@users.noreply.github.com>
2018-12-04 18:29:41 +0700

    RecyclerUserManager

--- app/src/main/java/com/keyroom/com/keyroom/Admin/user_manager.java
+++ app/src/main/java/com/keyroom/com/keyroom/Admin/user_manager.java
@@ -2,14 +2,61 @@ package com.keyroom.com.keyroom.Admin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
+import com.keyroom.com.keyroom.Adapter.RecyclerAdapterKelas;
+import com.keyroom.com.keyroom.Adapter.RecyclerAdapterUser;
+import com.keyroom.com.keyroom.Model.GetKelas;
+import com.keyroom.com.keyroom.Model.GetUser;
+import com.keyroom.com.keyroom.Model.Kelas;
+import com.keyroom.com.keyroom.Model.User;
 import com.keyroom.com.keyroom.R;
+import com.keyroom.com.keyroom.Rest.ApiClient;
+import com.keyroom.com.keyroom.Rest.ApiInterface;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class user_manager extends AppCompatActivity {
+    private ApiInterface mApiInterface;
+    private RecyclerView mRecyclerView;
+    private RecyclerAdapterUser mAdapter;
+    private List<User> mUser = new ArrayList<>();;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_manager);
+
+        initialize();
     }
+
+    private  void initialize(){
+        mApiInterface = ApiClient.getClient().create(ApiInterface.class);
+        mRecyclerView = findViewById(R.id.recyler_user_manager);
+
+        Call<GetUser> getUser = mApiInterface.getUser();
+        getUser.enqueue(new Callback<GetUser>() {
+            @Override
+            public void onResponse(Call<GetUser> call, Response<GetUser> response) {
+                mUser = response.body().getAllUser();
+
+                mAdapter = new RecyclerAdapterUser(mUser,getApplicationContext());
+                mRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                mRecyclerView.setAdapter(mAdapter);
+            }
+
+            @Override
+            public void onFailure(Call<GetUser> call, Throwable t) {
+                Log.e("Retrofit Get", t.toString());
+            }
+        });
+    }
+
 }
