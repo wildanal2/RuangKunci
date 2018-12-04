fdb691ed67b018dea3c2b550ab13240c417c434b
Mardianaps <mputrisasmita@gmail.com>
2018-12-04 17:36:10 +0700

    membuat user_manager, merubah tampilan setting admin dan user
    membuat user_manager, merubah tampilan setting admin dan user.

---
+++ app/src/main/java/com/keyroom/com/keyroom/Admin/user_manager.java
@@ -0,0 +1,15 @@
+package com.keyroom.com.keyroom.Admin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.keyroom.com.keyroom.R;
+
+public class user_manager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_manager);
+    }
+}
